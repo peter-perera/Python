7d3383fa3d736e1933afdfdb5c174075114b24d3
peter-perera <peterperera@riccarton.school.nz>
2025-03-18 09:20:30 +1300

    Made list for storage.

---
+++ pizza.py
@@ -0,0 +1,8 @@
+"""This program will ask the user for the amount of pizzas that they want out of four choices. Then it will print the amount of pizzas per flavour/topping."""
+
+pizza_list = [] # list to store the amount of pizzas
+
+cheese = input("How many cheese pizzas do we want: ")
+chicken = input("How many chicken pizzas do we want: ")
+pepperoni = input("How many pepperoni pizzas do we want: ")
+veggie = input("How many veggiepizzas do we want: ")
