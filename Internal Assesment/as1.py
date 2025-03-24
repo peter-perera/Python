5408494e7020344970bf03a65128a62aab84924e
peter-perera <peterperera@riccarton.school.nz>
2025-03-25 10:10:40 +1300

    Go through all the speeds to check if there are over or under the safe speed.

--- Internal Assesment/as1.py
+++ Internal Assesment/as1.py
@@ -3,10 +3,28 @@ Finally at the end of the program, it will go through all the speeds in the list
 
 # Creating a list to store the speeds. 
 speed = []
+highspeeds = 0 
 HIGH = 10  # maximum safe speed.
-SMALL = -1
+SMALL = 0
 stop = 'done' # stops program if the user inputs 'done'.
-meter_sec = float(('Input descent speed in m/s: '))
+meter_sec = input('Input descent speed in m/s: ')
 while meter_sec != stop:
     try:
-        
+        meter_sec = meter_sec.lower()
+        if meter_sec.strip('.').isnumeric():
+            speed.append(float(meter_sec))
+        else:
+            print('Error, invalid input.')
+        meter_sec = input('Input descent speed in m/s: ')
+        meter_sec = meter_sec.lower()
+    except ValueError:
+        print('Error, invalid input.')
+for item in speed:
+    if item > HIGH:
+        highspeeds = highspeeds + 1
+print(f"There were {highspeeds} space probes faster than the safe speed.")
+if highspeeds > SMALL:
+    print('The unsafe speeds are')
+    for item in speed:
+        if item > HIGH:
+            print(item)
